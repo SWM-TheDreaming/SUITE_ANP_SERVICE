f3f037f02c5ad31a3418bc3545776134fba9d772
김대현 <zxz4641@gmail.com>
2023-08-24 16:17:22 +0900

    init project

---
+++ src/main/java/com/suite/suite_anp_service/SuiteAnpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.suite.suite_anp_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuiteAnpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuiteAnpServiceApplication.class, args);
+	}
+
+}
