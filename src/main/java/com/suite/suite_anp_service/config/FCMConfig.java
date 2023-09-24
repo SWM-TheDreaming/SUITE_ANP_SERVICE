7a5f14599600c79129c8f85e14d3a32003aabc38
반영환 <lopahn2@gmail.com>
2023-09-24 21:33:09 +0900

    error: json 파일 log 수집

--- src/main/java/com/suite/suite_anp_service/config/FCMConfig.java
+++ src/main/java/com/suite/suite_anp_service/config/FCMConfig.java
@@ -1,23 +1,29 @@
 package com.suite.suite_anp_service.config;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+
+import com.google.gson.stream.JsonReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Configuration
 public class FCMConfig {
 
-
+    private static final Logger logger = LoggerFactory.getLogger(FCMConfig.class);
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
         ClassPathResource resource = new ClassPathResource("firebase/suite-firebase-admin.json");
@@ -32,9 +38,17 @@ public class FCMConfig {
                     firebaseApp = app;
             }
         } else {
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(refreshToken)).build();
-            firebaseApp = FirebaseApp.initializeApp(options);
+            try {
+                JsonReader reader = new JsonReader(new InputStreamReader(refreshToken, StandardCharsets.UTF_8));
+                reader.setLenient(true);
+
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(refreshToken)).build();
+                firebaseApp = FirebaseApp.initializeApp(options);
+            } catch (Exception e) {
+                logger.error("FirebaseApp 초기화 중 오류 발생", e);
+            }
+
         }
         return FirebaseMessaging.getInstance(firebaseApp);
     }
