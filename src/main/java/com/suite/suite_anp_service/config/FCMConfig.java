2727541da3a265c3800300d4c3474041f7ac8484
반영환 <lopahn2@gmail.com>
2023-09-24 21:07:20 +0900

    error: json 파일 log 수집

--- src/main/java/com/suite/suite_anp_service/config/FCMConfig.java
+++ src/main/java/com/suite/suite_anp_service/config/FCMConfig.java
@@ -6,23 +6,22 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
-import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
 
 @Configuration
 public class FCMConfig {
+
+
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
         ClassPathResource resource = new ClassPathResource("firebase/suite-firebase-admin.json");
-
-        //InputStream refreshToken = resource.getInputStream();
+        InputStream refreshToken = resource.getInputStream();
 
         FirebaseApp firebaseApp = null;
         List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
@@ -33,18 +32,9 @@ public class FCMConfig {
                     firebaseApp = app;
             }
         } else {
-            try {
-                InputStreamReader reader = new InputStreamReader(resource.getInputStream());
-                Gson gson = new Gson();
-                FirebaseOptions options = gson.fromJson(reader, FirebaseOptions.class);
-//                FirebaseOptions options = FirebaseOptions.builder()
-//                        .setCredentials(GoogleCredentials.fromStream(refreshToken)).build();
-                firebaseApp = FirebaseApp.initializeApp(options);
-
-            } catch(Exception e) {
-                throw new RuntimeException("Failed to read Firebase Admin SDK JSON file", e);
-            }
-
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(refreshToken)).build();
+            firebaseApp = FirebaseApp.initializeApp(options);
         }
         return FirebaseMessaging.getInstance(firebaseApp);
     }
