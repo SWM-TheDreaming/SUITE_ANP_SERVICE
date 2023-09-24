6df8e61e83ff53e6866d3a0d8ca5b0b5215e2d7d
반영환 <lopahn2@gmail.com>
2023-09-24 21:03:38 +0900

    error: json 파일 log 수집

--- src/main/java/com/suite/suite_anp_service/config/FCMConfig.java
+++ src/main/java/com/suite/suite_anp_service/config/FCMConfig.java
@@ -6,35 +6,23 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 @Configuration
 public class FCMConfig {
-    private boolean isValidJson(InputStream jsonStream) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            // Try to parse the JSON
-            JsonNode jsonNode = objectMapper.readTree(jsonStream);
-            // Successfully parsed, so it's a valid JSON
-            return true;
-        } catch (IOException e) {
-            // Failed to parse, it's not a valid JSON
-            return false;
-        }
-    }
-
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
         ClassPathResource resource = new ClassPathResource("firebase/suite-firebase-admin.json");
-        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-        System.out.println(resource);
-        InputStream refreshToken = resource.getInputStream();
+
+        //InputStream refreshToken = resource.getInputStream();
 
         FirebaseApp firebaseApp = null;
         List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
@@ -45,9 +33,18 @@ public class FCMConfig {
                     firebaseApp = app;
             }
         } else {
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(refreshToken)).build();
-            firebaseApp = FirebaseApp.initializeApp(options);
+            try {
+                InputStreamReader reader = new InputStreamReader(resource.getInputStream());
+                Gson gson = new Gson();
+                FirebaseOptions options = gson.fromJson(reader, FirebaseOptions.class);
+//                FirebaseOptions options = FirebaseOptions.builder()
+//                        .setCredentials(GoogleCredentials.fromStream(refreshToken)).build();
+                firebaseApp = FirebaseApp.initializeApp(options);
+
+            } catch(Exception e) {
+                throw new RuntimeException("Failed to read Firebase Admin SDK JSON file", e);
+            }
+
         }
         return FirebaseMessaging.getInstance(firebaseApp);
     }
