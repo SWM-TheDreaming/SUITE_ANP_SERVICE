3ba27fd40720e6a14458ced8718a35401ded953c
반영환 <lopahn2@gmail.com>
2023-09-25 02:28:09 +0900

    error: json 파일 log 수집

--- src/main/java/com/suite/suite_anp_service/config/FCMConfig.java
+++ src/main/java/com/suite/suite_anp_service/config/FCMConfig.java
@@ -7,48 +7,39 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import java.util.List;
 
 @Configuration
-@Slf4j
 public class FCMConfig {
-
 
     @Bean
     FirebaseMessaging firebaseMessaging() throws IOException {
-        try {
-            ClassPathResource resource = new ClassPathResource("firebase/suite-firebase-admin.json");
-            InputStream refreshToken = resource.getInputStream();
-            log.info("token info @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@", refreshToken);
 
-            FirebaseApp firebaseApp = null;
-            List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
+        ClassPathResource resource = new ClassPathResource("firebase/suite-firebase-admin.json");
+        InputStream refreshToken = resource.getInputStream();
+        FirebaseApp firebaseApp = null;
+        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
 
-            if(firebaseAppList != null && !firebaseAppList.isEmpty()) {
-                for(FirebaseApp app : firebaseAppList) {
-                    if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME))
-                        firebaseApp = app;
-                }
-            } else {
-                FirebaseOptions options = FirebaseOptions.builder()
-                        .setCredentials(GoogleCredentials.fromStream(refreshToken)).build();
-                firebaseApp = FirebaseApp.initializeApp(options);
+        if(firebaseAppList != null && !firebaseAppList.isEmpty()) {
+            for(FirebaseApp app : firebaseAppList) {
+                if(app.getName().equals(FirebaseApp.DEFAULT_APP_NAME))
+                    firebaseApp = app;
             }
-            return FirebaseMessaging.getInstance(firebaseApp);
-        } catch (Exception e) {
-            log.error(String.valueOf(e));
+        } else {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(refreshToken)).build();
+            firebaseApp = FirebaseApp.initializeApp(options);
         }
-        return null;
+        return FirebaseMessaging.getInstance(firebaseApp);
     }
 
 }
