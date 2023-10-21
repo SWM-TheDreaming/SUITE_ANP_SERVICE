0cf24a73c13c5aa8bcc49f49c02590d89699d461
반영환 <lopahn2@gmail.com>
2023-10-22 01:33:54 +0900

    feat: entityScan append

--- src/main/java/com/suite/suite_anp_service/SuiteAnpServiceApplication.java
+++ src/main/java/com/suite/suite_anp_service/SuiteAnpServiceApplication.java
@@ -2,6 +2,7 @@ package com.suite.suite_anp_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
@@ -15,6 +16,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 		"com.suite.suite_anp_service.alarm",
 		"com.suite.suite_anp_service.payment"
 })
+@EntityScan(basePackages = {"com.suite.suite_anp_service.anp.entity", "com.suite.suite_anp_service.alarm.entity", "com.suite.suite_anp_service.payment.entity"})
 public class SuiteAnpServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SuiteAnpServiceApplication.class, args);
